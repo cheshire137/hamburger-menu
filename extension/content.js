2927a15707789e5f1c4e0d240fc0fddab61a7c89
Sarah Vessels <cheshire137@github.com>
2016-06-14 23:12:16 -0500

    Replace glyphicons-menu-hamburger

--- extension/content.js
+++ extension/content.js
@@ -4,9 +4,14 @@ const faIcons = document.querySelectorAll('.fa-bars, .fa-navicon, .fa-reorder');
 // Octicons
 const octicons = document.querySelectorAll('.octicon-three-bars');
 
-const allIcons = Array.from(faIcons).concat(Array.from(octicons));
+// Glyphicons
+const glyphicons = document.querySelectorAll('.glyphicons-menu-hamburger');
+
+const allIcons = Array.from(faIcons).concat(Array.from(octicons)).
+                       concat(Array.from(glyphicons));
 allIcons.forEach(icon => {
   icon.style.width = `${icon.clientWidth}px`;
   icon.style.height = `${icon.clientHeight}px`;
   icon.classList.add('hamburgled');
+  icon.classList.add('deliciously-hamburgled');
 });
