41eb1ee33098a3b2f067264e4736162514360518
Sarah Vessels <cheshire137@github.com>
2016-06-14 23:06:25 -0500

    Replace octicon-three-bars

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,10 @@
-const faIcons = document.querySelectorAll('.fa-bars');
-const allIcons = Array.from(faIcons);
+// Font Awesome
+const faIcons = document.querySelectorAll('.fa-bars, .fa-navicon, .fa-reorder');
+
+// Octicons
+const octicons = document.querySelectorAll('.octicon-three-bars');
+
+const allIcons = Array.from(faIcons).concat(Array.from(octicons));
 allIcons.forEach(icon => {
   icon.style.width = `${icon.clientWidth}px`;
   icon.style.height = `${icon.clientHeight}px`;
