a54e09301e2e807aba60c2a5b7759ea497942bca
Sarah Vessels <cheshire137@github.com>
2016-06-15 00:10:54 -0500

    Style fi-list from Foundation

--- extension/content.js
+++ extension/content.js
@@ -1,30 +1,26 @@
-// Font Awesome
-const faIcons = document.querySelectorAll('.fa-bars, .fa-navicon, .fa-reorder');
+(function() {
+  // Font Awesome
+  const faIcons = document.querySelectorAll('.fa-bars, .fa-navicon, .fa-reorder');
 
-// Octicons
-const octicons = document.querySelectorAll('.octicon-three-bars, .octicon-grabber');
+  // Octicons
+  const octicons = document.querySelectorAll('.octicon-three-bars, .octicon-grabber');
 
-// Glyphicons
-const glyphicons = document.querySelectorAll('.glyphicons-menu-hamburger, .halflings-menu-hamburger, .glyphicon-menu-hamburger');
+  // Glyphicons
+  const glyphicons = document.querySelectorAll('.glyphicons-menu-hamburger, .halflings-menu-hamburger, .glyphicon-menu-hamburger');
 
-// Material Icons
-const materialIcons =
-    Array.from(document.querySelectorAll('.material-icons')).filter(icon => {
-      return icon.textContent.trim() === 'reorder';
-    });
+  // Material Icons
+  const materialIcons =
+      Array.from(document.querySelectorAll('.material-icons')).filter(icon => {
+        return icon.textContent.trim() === 'reorder';
+      });
 
-// Ionicons
-const ionicons = document.querySelectorAll('.ion-navicon-round, .ion-navicon, .ion-drag');
+  // Ionicons
+  const ionicons = document.querySelectorAll('.ion-navicon-round, .ion-navicon, .ion-drag');
 
-const allIcons = Array.from(faIcons).
-                       concat(Array.from(octicons)).
-                       concat(Array.from(glyphicons)).
-                       concat(materialIcons).
-                       concat(Array.from(ionicons));
-allIcons.forEach(icon => {
-  let width = icon.clientWidth;
-  let height = icon.clientHeight;
-  if (width > 0 && height > 0) {
+  // Foundation
+  const foundIcons = document.querySelectorAll('.fi-list');
+
+  function removePadding(icon, width, height) {
     let style;
     try {
       style = window.getComputedStyle(icon);
@@ -43,9 +39,44 @@ allIcons.forEach(icon => {
     if (style.paddingBottom) {
       height -= parseFloat(style.paddingBottom.replace(/px$/, ''));
     }
-    icon.style.width = `${width}px`;
-    icon.style.height = `${height}px`;
+    return [width, height];
   }
-  icon.classList.add('hamburgled');
-  icon.classList.add('deliciously-hamburgled');
-});
+
+  function squareItUp(width, height) {
+    if (width > 0 && height <= 0) {
+      height = width;
+    } else if (height > 0 && width <= 0) {
+      width = height;
+    }
+    return [width, height];
+  }
+
+  function setHamburgerWidth(icon) {
+    let width = icon.clientWidth;
+    let height = icon.clientHeight;
+    [width, height] = removePadding(icon, width, height);
+    if (width <= 0 && height <= 0 && icon.parentNode) {
+      height = icon.parentNode.clientHeight;
+    }
+    [width, height] = squareItUp(width, height);
+    if (width > 0) {
+      icon.style.width = `${width}px`;
+    }
+    if (height > 0) {
+      icon.style.height = `${height}px`;
+    }
+    if (icon.style.display === '') {
+      icon.style.display = 'inline-block';
+    }
+    icon.classList.add('hamburgled');
+    icon.classList.add('deliciously-hamburgled');
+  }
+
+  const allIcons = Array.from(faIcons).
+                         concat(Array.from(octicons)).
+                         concat(Array.from(glyphicons)).
+                         concat(materialIcons).
+                         concat(Array.from(ionicons)).
+                         concat(Array.from(foundIcons));
+  allIcons.forEach(icon => setHamburgerWidth(icon));
+})()
