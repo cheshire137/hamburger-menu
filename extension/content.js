34ef35dfd7c9eba5b6d84b1683ed6496ca2b2078
Sarah Vessels <cheshire137@github.com>
2016-06-14 23:47:48 -0500

    Replace Ionians

--- extension/content.js
+++ extension/content.js
@@ -13,13 +13,36 @@ const materialIcons =
       return icon.textContent.trim() === 'reorder';
     });
 
-const allIcons = Array.from(faIcons).concat(Array.from(octicons)).
+// Ionicons
+const ionicons = document.querySelectorAll('.ion-navicon-round, .ion-navicon, .ion-drag');
+
+const allIcons = Array.from(faIcons).
+                       concat(Array.from(octicons)).
                        concat(Array.from(glyphicons)).
-                       concat(materialIcons);
+                       concat(materialIcons).
+                       concat(Array.from(ionicons));
 allIcons.forEach(icon => {
-  const width = icon.clientWidth;
-  const height = icon.clientHeight;
+  let width = icon.clientWidth;
+  let height = icon.clientHeight;
   if (width > 0 && height > 0) {
+    let style;
+    try {
+      style = window.getComputedStyle(icon);
+    } catch (err) {
+      style = {};
+    }
+    if (style.paddingLeft) {
+      width -= parseFloat(style.paddingLeft.replace(/px$/, ''));
+    }
+    if (style.paddingRight) {
+      width -= parseFloat(style.paddingRight.replace(/px$/, ''));
+    }
+    if (style.paddingTop) {
+      height -= parseFloat(style.paddingTop.replace(/px$/, ''));
+    }
+    if (style.paddingBottom) {
+      height -= parseFloat(style.paddingBottom.replace(/px$/, ''));
+    }
     icon.style.width = `${width}px`;
     icon.style.height = `${height}px`;
   }
