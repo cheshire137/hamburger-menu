80aa7c68760b6f85807cd34cc8bbaf7d2c58541b
Sarah Vessels <cheshire137@github.com>
2016-06-14 23:26:34 -0500

    Replace material-icons

--- extension/content.js
+++ extension/content.js
@@ -7,8 +7,15 @@ const octicons = document.querySelectorAll('.octicon-three-bars');
 // Glyphicons
 const glyphicons = document.querySelectorAll('.glyphicons-menu-hamburger, .halflings-menu-hamburger, .glyphicon-menu-hamburger');
 
+// Material Icons
+const materialIcons =
+    Array.from(document.querySelectorAll('.material-icons')).filter(icon => {
+      return icon.textContent.trim() === 'reorder';
+    });
+
 const allIcons = Array.from(faIcons).concat(Array.from(octicons)).
-                       concat(Array.from(glyphicons));
+                       concat(Array.from(glyphicons)).
+                       concat(materialIcons);
 allIcons.forEach(icon => {
   const width = icon.clientWidth;
   const height = icon.clientHeight;
