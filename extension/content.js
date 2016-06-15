da6839bcf4847492a97eddd991810051001b5fc0
Sarah Vessels <cheshire137@github.com>
2016-06-14 23:19:01 -0500

    Replace halflings-menu-hamburger

--- extension/content.js
+++ extension/content.js
@@ -5,13 +5,17 @@ const faIcons = document.querySelectorAll('.fa-bars, .fa-navicon, .fa-reorder');
 const octicons = document.querySelectorAll('.octicon-three-bars');
 
 // Glyphicons
-const glyphicons = document.querySelectorAll('.glyphicons-menu-hamburger');
+const glyphicons = document.querySelectorAll('.glyphicons-menu-hamburger, .halflings-menu-hamburger');
 
 const allIcons = Array.from(faIcons).concat(Array.from(octicons)).
                        concat(Array.from(glyphicons));
 allIcons.forEach(icon => {
-  icon.style.width = `${icon.clientWidth}px`;
-  icon.style.height = `${icon.clientHeight}px`;
+  const width = icon.clientWidth;
+  const height = icon.clientHeight;
+  if (width > 0 && height > 0) {
+    icon.style.width = `${width}px`;
+    icon.style.height = `${height}px`;
+  }
   icon.classList.add('hamburgled');
   icon.classList.add('deliciously-hamburgled');
 });
